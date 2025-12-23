372cdd9ce1858e112e46e0736f636e138972d4d4
sachinkn92 <130657745+sachinkn92@users.noreply.github.com>
2025-12-23 23:10:07 +0530

    Add registerProduct function to handle product registration

--- ERORR.js
+++ ERORR.js
@@ -231,6 +231,12 @@ try {
     }
   };
 
+ const registerProduct = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setResult(null);
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         if len(nums1) > len(nums2):
