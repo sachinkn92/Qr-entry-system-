9bf7e5f0cd8804d9f7d3cc98b486a8d05d33f336
sachinkn92 <130657745+sachinkn92@users.noreply.github.com>
2025-12-25 00:23:42 +0530

    Add package dependencies to postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -18,3 +18,28 @@ module.exports = {
     "autoprefixer": "^10.4.14"
   }
 }
+
+
+
+
+{
+  "name": "qr-entry-system",
+  "version": "0.1.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "qr-entry-system",
+      "version": "0.1.0",
+      "dependencies": {
+        "@supabase/supabase-js": "^2.27.0",
+        "autoprefixer": "^10.4.14",
+        "html5-qrcode": "^2.3.8",
+        "next": "13.5.8",
+        "nodemailer": "^6.9.4",
+        "postcss": "^8.4.24",
+        "qrcode": "1.5.1",
+        "react": "18.2.0",
+        "react-dom": "18.2.0",
+        "tailwindcss": "^3.4.7"
+      }
